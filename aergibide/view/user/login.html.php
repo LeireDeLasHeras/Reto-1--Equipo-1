c67a9fd41179a0683dd2ca003c948bdacd35e130
Leire de las Heras <leire.delasheras@ikasle.egibide.org>
2024-10-29 08:43:55 +0100

    Login solucionado (creo)

--- aergibide/view/user/login.html.php
+++ aergibide/view/user/login.html.php
@@ -1,25 +1,26 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesi&oacute;n</title>
-    <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="assets/css/login_style.css">
-</head>
-<body>
-    <main>
-        <div class="logo">
-            <img class="logo_aergibide" src="assets/img/logo_aergibide.png" alt="Logo Aergibide">
-        </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Iniciar sesión</title>
+        <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="assets/css/login_style.css">
+    </head>
+    <body>
+        <main>
+            <div class="logo">
+                <img class="logo_aergibide" src="assets/img/logo_aergibide.png" alt="Logo Aergibide">
+            </div>
 
-        <form action="" method="POST">
-            <label for="correo">Correo electrónico:</label>
-            <input type="text" id="correo" class="input_correo" name="correo" required>
+            <form action="" method="POST">
+                <label for="correo">Correo electrónico:</label>
+                <input type="text" id="correo" class="input_correo" name="correo" required>
 
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" class="input_password" name="password" required>
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" class="input_password" name="password" required>
 
-            <input type="submit" class="boton_login" value="Iniciar Sesión">
-            <a href="index.php?controller=user&action=register" class="boton_registro">Registrarme</a>
-        </form>
-    </main>
-</body>
+                <!-- Añadir name="submit" al botón de envío para que se detecte en el controlador -->
+                <input type="submit" name="submit" class="boton_login" value="Iniciar Sesión">
+                <a href="index.php?controller=user&action=register" class="boton_registro">Registrarme</a>
+            </form>
+        </main>
+    </body>
