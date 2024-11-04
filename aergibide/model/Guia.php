63a402cae534a7110e400447f02a5d17e3489c74
Oier <oier.albeniz@ikasle.egibide.org>
2024-11-04 10:50:27 +0100

    Crear guias corregido y listar guias con el boton descarga

--- aergibide/model/Guia.php
+++ aergibide/model/Guia.php
@@ -32,16 +32,14 @@ class Guia
     }
 
     public function crearGuia($param) {
-        $titulo = $descripcion = $tema = $filePath = null;
-    
-        if (isset($param['file_path'])) $filePath = $param['file_path'];
-    
-        $param = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
-    
-        if (isset($param["titulo"])) $titulo = $param["titulo"];
-        if (isset($param["descripcion"])) $descripcion = $param["descripcion"];
+     
+        $titulo = isset($param['titulo']) ? $param['titulo'] : '';
+        $descripcion = isset($param['descripcion']) ? $param['descripcion'] : '';
+        $tema = isset($param['tema']) ? $param['tema'] : '';
+        $filePath = isset($param['file_path']) ? $param['file_path'] : '';
+
         $idUsuario = $_SESSION['user_data']['idUsuario'];
-    
+   
         try {
             $sql = "INSERT INTO Guia (titulo, descripcion, fecha, idUsuario, fichero) VALUES (?, ?, ?, ?, ?)";
             $stmt = $this->connection->prepare($sql);
